61fda82fc7e13ec201c8cb21d68da6a188bf18f0
xxfariddx <qarayevfarid3@gmail.com>
2026-07-18 16:45:22 +0400

    es6

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,13 +1,13 @@
 export default function updateUniqueItems(map) {
-  if (!(map instanceof Map)) {
-    throw new Error('Cannot process');
-  }
-
-  map.forEach((quantity, name) => {
-    if (quantity === 1) {
-      map.set(name, 100);
+    if (!(map instanceof Map)) {
+        throw new Error('Cannot process');
     }
-  });
 
-  return map;
+    for (const [key, value] of map) {
+        if (value === 1) {
+            map.set(key, 100);
+        }
+    }
+
+    return map;
 }
